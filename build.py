e4e7b0aa7239991bcb3460c7379c2e92b23a40eb
Bastian Schroeder <bs.medienspot@gmailcom>
2020-05-28 13:42:17 +0200

    initial

---
+++ build.py
@@ -0,0 +1,104 @@
+import os
+from datetime import datetime
+from jinja2 import Environment, PackageLoader
+from markdown2 import markdown
+import configparser
+import sass
+import rcssmin
+
+#initiate configs
+config = configparser.ConfigParser()
+config.read('site_config.ini')
+
+#initiate site meta
+site_meta = {
+    'domain' : config['DEFAULT']['Domain'],
+    'posts_pre_slug' : config['DEFAULT']['Posts Pre Slug']
+}
+
+# Compile and write main.scss to main.css into output/css
+css_string = sass.compile(filename="assets/css/main.scss")
+css_file_path = "output/css/main.css"
+os.makedirs(os.path.dirname(css_file_path), exist_ok=True)
+with open(css_file_path, 'w') as file:
+    file.write(rcssmin.cssmin(css_string))
+
+# Reading Posts
+POSTS = {}
+for markdown_post in os.listdir('content/posts'):
+    file_path = os.path.join('content/posts', markdown_post)
+
+    with open(file_path, 'r') as file:
+        POSTS[markdown_post] = markdown(file.read(), extras=['metadata'])
+
+# Reading Static Sites
+STATICS = {}
+for markdown_static in os.listdir('content/static'):
+    file_path = os.path.join('content/static', markdown_static)
+
+    with open(file_path, 'r') as file:
+        STATICS[markdown_static] = markdown(file.read(), extras=['metadata'])
+
+# Sorting Posts
+POSTS = {
+    post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
+}
+
+#loading templates
+env = Environment(loader=PackageLoader('build', 'templates'))
+home_template = env.get_template('home.html')
+post_template = env.get_template('post.html')
+static_template = env.get_template('statics.html')
+
+#rendering homepage
+posts_metadata = [POSTS[post].metadata for post in POSTS]
+tags = [post['tags'] for post in posts_metadata]
+home_html = home_template.render(posts=posts_metadata, tags=tags, site_meta=site_meta)
+
+#writing homepage
+with open('output/index.html', 'w') as file:
+    file.write(home_html)
+
+#writing posts
+for post in POSTS:
+    post_metadata = POSTS[post].metadata
+
+    #read meta
+    post_data = {
+        'content': POSTS[post],
+        'title': post_metadata['title'],
+        'date': post_metadata['date'],
+        'summary': post_metadata['summary'],
+        'slug': post_metadata['slug']
+    }
+
+    #render post
+    post_html = post_template.render(post=post_data, site_meta=site_meta)
+
+    post_file_path = 'output/{posts_pre_slug}/{slug}.html'.format(posts_pre_slug=site_meta['posts_pre_slug'], slug=post_metadata['slug'])
+
+    os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
+    with open(post_file_path, 'w') as file:
+        file.write(post_html)
+
+#writing static sites
+for static_site in STATICS:
+    static_metadata = STATICS[static_site].metadata
+
+    #read meta
+    static_data = {
+        'content': STATICS[static_site],
+        'title': static_metadata['title'],
+        'date': static_metadata['date'],
+        'summary': static_metadata['summary'],
+        'slug': static_metadata['slug']
+    }
+
+    #render static site
+    static_html = static_template.render(static=static_data, site_meta=site_meta)
+
+    static_file_path = 'output/{slug}.html'.format(slug=static_metadata['slug'])
+
+    os.makedirs(os.path.dirname(static_file_path), exist_ok=True)
+    with open(static_file_path, 'w') as file:
+        file.write(static_html)
---
+++ serve.py
@@ -0,0 +1,21 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+def handler_from(directory):
+    def _init(self, *args, **kwargs):
+        return http.server.SimpleHTTPRequestHandler.__init__(self, *args, directory=self.directory, **kwargs)
+    return type(f'HandlerFrom<{directory}>',
+                (http.server.SimpleHTTPRequestHandler,),
+                {'__init__': _init, 'directory': directory})
+
+with socketserver.TCPServer(("", PORT), handler_from("output")) as httpd:
+    print("serving at port", PORT)
+    print("Close Server with CTRL-c")
+    try: 
+        httpd.serve_forever(); 
+    except KeyboardInterrupt: 
+        pass; 
+    httpd.server_close()
+    print(" Server closed")

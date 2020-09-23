8f345613fab5e22da338dfecf3095d8819ab6249
Ubuntu <ubuntu@ip-172-26-4-181.eu-central-1.compute.internal>
2020-09-23 12:30:40 +0000

    zeug

--- serve.py
+++ serve.py
@@ -1,16 +1,22 @@
 import http.server
 import socketserver
+import os
 
-PORT = 8050
+PORT = 8081
 
 def handler_from(directory):
     def _init(self, *args, **kwargs):
+        print(self.directory)
         return http.server.SimpleHTTPRequestHandler.__init__(self, *args, directory=self.directory, **kwargs)
     return type(f'HandlerFrom<{directory}>',
                 (http.server.SimpleHTTPRequestHandler,),
                 {'__init__': _init, 'directory': directory})
+                
+print(os.getcwd())
+os.chdir("output")                
+Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("0.0.0.0", PORT), handler_from("output")) as httpd:
+with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
     print("serving at port", PORT)
     print("Close Server with CTRL-c")
     try: 

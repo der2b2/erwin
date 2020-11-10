56977db54fbf4a1524ebd4d92fea55e7306ab666
Bastian Schroeder <b.schroeder@medienspot.de>
2020-11-10 08:59:28 +0000

    responsive image creation only if generated images not found

--- _erwin/image_processor.py
+++ _erwin/image_processor.py
@@ -4,6 +4,7 @@ from PIL import Image
 import threading, queue
 from _thread import start_new_thread
 import psutil
+import glob
 
 def foo():
     print("image foo")
@@ -35,14 +36,18 @@ def generate_responsive_images_helper(from_path, to_path, images, image_sizes, m
     file_path = os.path.join(from_path, images)
 
     image_name = images.split(".")[0]
-    m_image = Image.open(file_path)
-    for image_size in image_sizes:
-        new_image = m_image.copy()
-        new_image.thumbnail((int(image_size),int(image_size)))
-        image_file_path_webp = to_path + '{name}-{size}.webp'.format(name=image_name, size=image_size)
-        image_file_path_jpg = to_path + '{name}-{size}.jpg'.format(name=image_name, size=image_size)
-        new_image.save(image_file_path_webp, quality=50, method=0)
-        new_image.save(image_file_path_jpg, quality=50, optimize=True)
+    
+    if len(glob.glob(to_path + image_name + "*")) != len(image_sizes)*2:
+        m_image = Image.open(file_path)
+        for image_size in image_sizes:
+            new_image = m_image.copy()
+            new_image.thumbnail((int(image_size),int(image_size)))
+            image_file_path_webp = to_path + '{name}-{size}.webp'.format(name=image_name, size=image_size)
+            image_file_path_jpg = to_path + '{name}-{size}.jpg'.format(name=image_name, size=image_size)
+            new_image.save(image_file_path_webp, quality=50, method=0)
+            new_image.save(image_file_path_jpg, quality=50, optimize=True)
+    else:
+        print("Image with name " + image_name + " generated before, skipping. To regenerate please execute clean.py and repeat.")
     if my_que:
         my_que.put("Done")
 
